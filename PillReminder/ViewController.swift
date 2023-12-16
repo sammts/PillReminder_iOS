eae052cbffd0abbbb846cd9fb5f592575b29a37e
sammts <samueltapia@comunidad.unam.mx>
2023-12-16 06:55:46 -0600

    Initial Commit

---
+++ PillReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PillReminder
+//
+//  Created by Samuel Tapia on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
